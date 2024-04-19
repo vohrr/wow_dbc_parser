b6c17755f7e3a644cde02a077d7d28c30ebc24f4
vohrr <matthew.d.pingel@gmail.com>
2024-04-19 08:31:56 -0400

    error handling for parser, file cleanup in fail case

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import requests
 import csv
 import json
+import os
 
 BETA_REPO = 'https://raw.githubusercontent.com/Vladinator/wow-dbc-archive/release/wow_beta_latest'
 LIVE_REPO = 'https://raw.githubusercontent.com/Vladinator/wow-dbc-archive/release/wow_latest'
@@ -9,21 +10,29 @@ PTR_REPO = 'https://raw.githubusercontent.com/Vladinator/wow-dbc-archive/release
 
 def get_file_from_repo(version:str, file_name:str):
     requestUrl = f"{version}/{file_name}"
+    
     print(f"Fetching data from {file_name}...")
+    
     with open(f"./json/{file_name.replace(".csv","")}.json", "w") as jsonfile:
+       
         with (requests.get(requestUrl, stream=True)) as response:
-            lines = (line.decode('utf-8') for line in response.iter_lines())
-            data = []
-            for row in csv.DictReader(lines):
-                data.append(row)
+       
             if response.ok:
+                lines = (line.decode('utf-8') for line in response.iter_lines())
+                data = []
+                for row in csv.DictReader(lines):
+                    data.append(row)
+                json.dump(data, jsonfile)
+       
                 print(f"Done. response code: {response.status_code}, {response.elapsed} elapsed.")
             else:
+                jsonfile.close()
+                os.remove(f"./json/{file_name.replace(".csv","")}.json")
+                
                 print(f"Error retrieving data from {file_name}. Response: {response.status_code} {response.reason}")
-            json.dump(data, jsonfile) 
-
+            
 def main():
-    get_file_from_repo(LIVE_REPO, "spell.csv")
+    get_file_from_repo(LIVE_REPO, "talenttree.csv")
 
 
 
