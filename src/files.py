f344b425c658fbc59f5ee2b7babcee2864a6bb6d
vohrr <matthew.d.pingel@gmail.com>
2024-04-20 21:05:08 -0400

    add filename constants

---
+++ src/files.py
@@ -0,0 +1,6 @@
+
+SPELL_FILES = ['spell','spellcasttimes','spelleffect','spellmisc','spellname','spellcooldowns','spellduration','spellpower','specializationspells']
+
+SPELL_FILES_OPTIONAL = ['spellcategory','spellcategories','spellprocsperminute','spellprocsperminutemod','spelllabel','']
+
+TALENT_FILES = ['talent']
